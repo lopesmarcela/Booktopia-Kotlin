1ee43639e4b155fda0e87aaffafb8990a60e85b8
MARCELA LOPES <marcela.lopes@atos.net>
2022-09-01 17:38:35 -0300

    Aplicação Inicial

---
+++ src/test/kotlin/com/booktopia/BooktopiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.booktopia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BooktopiaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
