b4f3c4aaa13744fecef8a635a76f5571a95a5093
MARCELA LOPES <marcela.lopes@atos.net>
2022-09-02 11:21:55 -0300

    adicionando annotations na model

--- src/main/kotlin/com/booktopia/models/ClientModel.kt
+++ src/main/kotlin/com/booktopia/models/ClientModel.kt
@@ -1,10 +1,23 @@
 package com.booktopia.models
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity(name = "clients")
 data class ClientModel (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var idClient: Int? = null,
+    @Column
     var cpf: String,
+    @Column
     var name: String,
+    @Column
     var email: String,
+    @Column
     var address: String
 ){
 }
