6b29cdd3180f8533a484a52a6e7bbc117bee35e9
MARCELA LOPES <marcela.lopes@atos.net>
2022-09-16 10:44:35 -0300

    Adicionando autenticacao no swagger

--- src/main/kotlin/com/booktopia/config/SwaggerConfig.kt
+++ src/main/kotlin/com/booktopia/config/SwaggerConfig.kt
@@ -1,16 +1,24 @@
 package com.booktopia.config
 
+import io.swagger.models.auth.In
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.RequestMethod
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.builders.ResponseMessageBuilder
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
 import springfox.documentation.service.ResponseMessage
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+
 
 @Configuration
 @EnableSwagger2
@@ -25,10 +33,23 @@ class SwaggerConfig {
             .useDefaultResponseMessages(false)
             .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
 
-            .apiInfo(ApiInfoBuilder()
-                .title("Booktopia")
-                .description("Sistema gerenciador de aluguel de livros")
-                .build())
+            .apiInfo(
+                ApiInfoBuilder()
+                    .title("Booktopia")
+                    .description("Sistema gerenciador de aluguel de livros")
+                    .build()
+            )
+
+            .securitySchemes(
+                Arrays.asList(
+                    ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name)
+                )
+            )
+            .securityContexts(
+                Arrays.asList(securityContext())
+            );
+
+
 
     }
 
@@ -56,4 +77,20 @@ class SwaggerConfig {
             }
         }
     }
+    private fun securityContext(): SecurityContext? {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .forPaths(PathSelectors.regex("^(?!auth).*$"))
+            .build()
+    }
+
+    fun defaultAuth(): List<SecurityReference?>? {
+        val authorizationScope = AuthorizationScope("ADMIN", "accessEverything")
+        val authorizationScopes: Array<AuthorizationScope?> = arrayOfNulls<AuthorizationScope>(1)
+        authorizationScopes[0] = authorizationScope
+        return Arrays.asList(
+            SecurityReference("Token Access", authorizationScopes)
+        )
+    }
+
 }
